8051a6e4fce61a0de12f6eebd2751dff1ba39039
Jašo <andrija.pericic@gmail.com>
2025-09-03 20:48:55 +0200

    zad_02.py

--- zad_02.py
+++ zad_02.py
@@ -1,10 +1,5 @@
     
-'''Stranice a i b, četverokuta te za površinu tog četverokuta.
-    
-    Izračun mjesečne potrošnje el. struje te cijene el. struje koju potroši 
-    mikrovalna pećnica snage 1,3 kW ako se koristi 2 sata dnevno?
-    
-    Stranice trokuta, površinu trokuta (P = (𝑎 ∗ 𝑣_𝑎)/2, 𝑣_𝑎 je visina na stranicu a) te opseg trokuta.'''
+'''Stranice a i b, četverokuta te za površinu tog četverokuta. '''
 
 
 
---
+++ zad_03.py
@@ -0,0 +1,9 @@
+''' Izračun mjesečne potrošnje el. struje te cijene el. struje koju potroši 
+    mikrovalna pećnica snage 1,3 kW ako se koristi 2 sata dnevno?'''
+
+
+
+power = 1.3
+daly_comsuption = 2
+    
+  
