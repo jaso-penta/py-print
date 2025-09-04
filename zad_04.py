76d62c4c9985d1b80e3f30b99e5d54ed622ba2c9
Jašo <andrija.pericic@gmail.com>
2025-09-04 20:53:13 +0200

    Create zad_04.py

---
+++ zad_04.py
@@ -0,0 +1,14 @@
+'''Stranice trokuta, površinu trokuta (P = (𝑎 ∗ 𝑣_𝑎)/2, 𝑣_𝑎 je visina na stranicu a) te opseg trokuta.'''
+
+
+a = 10
+b = 4
+c = 7
+v_a = 5
+
+p = a * v_a / 2
+
+
+print()
+print('Povrsina trokuta je:', p)
+print()
