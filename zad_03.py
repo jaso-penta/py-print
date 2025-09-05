a41ed5c0f9a5d8f511a3b5c9f0e7564dd2048e5e
Jašo <andrija.pericic@gmail.com>
2025-09-05 18:05:30 +0200

    Update zad_03.py

--- zad_03.py
+++ zad_03.py
@@ -8,9 +8,18 @@ power = 1.3 # kW
 hours_per_day = 2
 days_per_month = 30
 
-
+daily_consumption = power * hours_per_day
 monthly_consumption = power * hours_per_day * days_per_month
+
 price_by_month = monthly_consumption * el_price
+daily_price = daily_consumption * el_price
+
+
+print('Dnevna potrosnja:', daily_consumption)
+print('Dnevna cijena:', daily_price)
+print('Mjesecna cijena:', price_by_month)
+
+
 
 print()
 print(f'Mjesecna potrosnja el. energije je: {monthly_consumption}, cijena potrosnje je: {price_by_month}')
